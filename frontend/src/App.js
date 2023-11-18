06072f4818f8535e06bbfe4f26ae67c781673b0c
Abhishek Kumar <anu250867anu@gmail.com>
2023-11-18 18:18:45 +0530

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import FileUpload from "./Components/FileUpload";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>File Upload </h1>
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
